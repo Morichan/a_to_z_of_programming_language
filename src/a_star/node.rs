4bd41efdba638fc774bd47cc00d96fc9c381c09a
Morichan <nintendo698@gmail.com>
2018-10-31 16:52:27 +0900

    feat: NodeクラスにPoint構造体を追加

--- src/a_star/node.rs
+++ src/a_star/node.rs
@@ -1,17 +1,33 @@
+use std::cell::UnsafeCell;
+
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
 pub struct Node {
-    pub id: i32,
+    id: i32,
+    point: UnsafeCell<Point>,
 }
 
 impl Node {
     pub fn new() -> Node {
-        Node {id: 0}
+        Node {id: 0, point: UnsafeCell::new(Point {x: 0, y: 0})}
+    }
+
+    pub fn set_id(&mut self, number: i32) {
+        self.id = number;
     }
 
     pub fn get_id(&self) -> i32 {
         self.id
     }
 
-    pub fn set_id(&mut self, number: i32) {
-        self.id = number;
+    pub fn set_point(&mut self, x: i32, y: i32) {
+        self.point = UnsafeCell::new(Point {x: x, y: y});
+    }
+
+    pub fn get_point(&mut self) -> *mut Point {
+        self.point.get()
     }
 }
--- tests/node.rs
+++ tests/node.rs
@@ -5,7 +5,7 @@ mod tests {
     use a_to_z_of_programming_language::a_star::node;
 
     #[test]
-    fn node_is_checked_id_when_node_was_set_id() {
+    fn checked_id_when_node_was_set_id() {
         let expected = 1;
         let mut obj = node::Node::new();
 
@@ -13,6 +13,17 @@ mod tests {
         let actual = obj.get_id();
 
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn checked_point_when_node_was_set_point() {
+        let expected = 1;
+        let mut obj = node::Node::new();
+
+        obj.set_point(1, 1);
+        let actual = obj.get_point();
+
+        assert_eq!(expected, unsafe{ (*actual).x });
     }
 }
 
